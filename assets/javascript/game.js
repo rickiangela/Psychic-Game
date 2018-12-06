61e36bcbe034ee25ad2a4d5d244924f6848a0639
rickiangela <rickiangela@gmail.com>
2018-12-06 16:35:06 -0500

    final product

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessesSoFar = [];
+
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+console.log("Computer: " + computerChoice);
+
+
+
+document.onkeypress = function (event) {
+
+var userGuess = event.key;
+
+console.log("User: " + userGuess);
+
+guessesSoFar.push(userGuess);
+
+console.log("Guesses so far:" +guessesSoFar);
+
+
+
+if (userGuess === computerChoice && guesses != 0){
+    
+    wins++;
+    guesses = 11;
+    guessesSoFar = [];
+
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    console.log("Computer: " + computerChoice);
+}
+if (userGuess != computerChoice && guesses != 0) {
+    guesses--;
+}
+
+if (guesses === 0) {
+    losses++
+    guesses = 10;
+    guessesSoFar = [];
+}
+
+document.getElementById('wins').innerHTML = "Wins: " + wins;
+document.getElementById('losses').innerHTML = "Losses: " + losses;
+document.getElementById('guesses-left').innerHTML = "Guesses left: " + guesses;
+document.getElementById('guess-so-far').innerHTML = "Guesses so far: " + guessesSoFar;
+
+}
